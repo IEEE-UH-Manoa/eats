794edeb2c3be94161675836d63613185066728ac
Kenny Luong <luong97@hawaii.edu>
2015-11-17 13:38:50 -1000

    Fixed accidental (broken) features that got in

--- eats.js
+++ eats.js
@@ -1,18 +1,20 @@
-var updateEat = function(data){
-  $('#eats').fadeOut("fast", function(){
-      $('#eats').text(data);
-      $('#eats').fadeIn("fast");
-  });
-}
+$(document).ready(function(){
+    var updateEat = function(data){
+      $('#eats').fadeOut("fast", function(){
+          $('#eats').text(data);
+          $('#eats').fadeIn("fast");
+      });
+    }
 
-$.get("http://api.ieeeatuhm.com/eat", updateEat);
+    $(document).keypress(function(e) {
+      if(e.which == 13) {
+        $.get("http://api.ieeeatuhm.com/eats", updateEat);
+      }
+    });
 
-$(document).keypress(function(e) {
-  if(e.which == 13) {
-    $.get("http://api.ieeeatuhm.com/eats", updateEat);
-  }
-});
+    $('#try_again').click(function(){
+        $.get("http://api.ieeeatuhm.com/eats", updateEat);
+    });
 
-$('#try_again').click(function(){
-    $.get("http://api.ieeeatuhm.com/eats", updateEat);
+    $.get("http://api.ieeeatuhm.com/eat", updateEat);
 });
